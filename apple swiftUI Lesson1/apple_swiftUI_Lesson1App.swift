2ad9ae86afc67b8b710c9f2ebdb5fe5bdb8a6eb1
okcreating <oksibearus@gmail.com>
2024-09-01 12:33:43 +0100

    Initial Commit

---
+++ apple swiftUI Lesson1/apple_swiftUI_Lesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  apple_swiftUI_Lesson1App.swift
+//  apple swiftUI Lesson1
+//
+//  Created by Oksana Kazarinova on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct apple_swiftUI_Lesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
