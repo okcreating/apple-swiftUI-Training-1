9d2971dd7a229cbff7f1c4785509c430957e01c8
okcreating <oksibearus@gmail.com>
2024-09-16 22:06:37 +0100

    model, images, func load

---
+++ apple swiftUI Lesson1/View/LandmarkRow.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkRow.swift
+//  apple swiftUI Lesson1
+//
+//  Created by Oksana Kazarinova on 16/09/2024.
+//
+
+import SwiftUI
+
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+#Preview {
+    LandmarkRow()
+}
