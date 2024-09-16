744d7aca7202a03452ede9c81796b607ed8e9664
okcreating <oksibearus@gmail.com>
2024-09-16 23:00:27 +0100

    navigation

---
+++ apple swiftUI Lesson1/View/LandmarkDetail.swift
@@ -0,0 +1,53 @@
+//
+//  LandmarkDetail.swift
+//  apple swiftUI Lesson1
+//
+//  Created by Oksana Kazarinova on 16/09/2024.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+                    MapView()
+                        .frame(height: 300)
+
+
+                    CircleImage()
+                        .offset(y: -130)
+                        .padding(.bottom, -130)
+
+
+                    VStack(alignment: .leading) {
+                        Text("Turtle Rock")
+                            .font(.title)
+
+
+                        HStack {
+                            Text("Joshua Tree National Park")
+                            Spacer()
+                            Text("California")
+                        }
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+
+
+                        Divider()
+
+
+                        Text("About Turtle Rock")
+                            .font(.title2)
+                        Text("Descriptive text goes here.")
+                    }
+                    .padding()
+
+
+                    Spacer()
+                }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
