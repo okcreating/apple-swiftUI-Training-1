0d260f714d613ba4c29b056bc019212ddd95d61d
okcreating <oksibearus@gmail.com>
2025-02-20 12:46:50 +0000

    Category item cell

---
+++ apple swiftUI Lesson1/View/CategoriesGroup/CategoryItem.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryItem.swift
+//  apple swiftUI Lesson1
+//
+//  Created by Oksana Kazarinova on 20/02/2025.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks[0])
+}
