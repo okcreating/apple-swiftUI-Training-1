de337693a90e1c213b5c3dacbd8308b9ae64453a
okcreating <oksibearus@gmail.com>
2025-02-25 21:41:57 +0000

    profile model & draft view

---
+++ apple swiftUI Lesson1/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  apple swiftUI Lesson1
+//
+//  Created by Oksana Kazarinova on 25/02/2025.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "g_kumar")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
---
+++ apple swiftUI Lesson1/View/Profiles/ProfileHost.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileHost.swift
+//  apple swiftUI Lesson1
+//
+//  Created by Oksana Kazarinova on 25/02/2025.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default //You set the draftProfile here to a default profile as a placeholder until you introduce the model data profile later on.
+    
+    var body: some View {
+        Text("Profile for: \(draftProfile.username)")
+    }
+}
+
+//The ProfileHost view will host both a static, summary view of profile information and an edit mode.
+
+#Preview {
+    ProfileHost()
+}
