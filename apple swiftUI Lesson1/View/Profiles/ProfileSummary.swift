c2dd5e46d5b2d05b35270af71ae832fbfa1d06d7
okcreating <oksibearus@gmail.com>
2025-02-26 15:33:28 +0000

    Badge added to summary

--- apple swiftUI Lesson1/View/Profiles/ProfileSummary.swift
+++ apple swiftUI Lesson1/View/Profiles/ProfileSummary.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ProfileSummary: View {
+    @Environment(ModelData.self) var modelData
     var profile: Profile
 
     var body: some View {
@@ -20,6 +21,35 @@ struct ProfileSummary: View {
                 Text("Notification: \(profile.prefersNotifications ? "On": "Off")")
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
                 Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+
+                Divider()
+                // add several badges with varying hues and reasons for earning the badge.
+
+                VStack(alignment: .leading) {
+                    Text("Completed badges")
+                        .font(.headline)
+
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                        }
+                    }
+
+                Divider()
+
+                VStack(alignment: .leading){
+                    Text("Recent hikes")
+                        .font(.headline)
+
+                    HikeView(hike: modelData.hikes[0])
+                }
             }
         }
     }
@@ -27,4 +57,5 @@ struct ProfileSummary: View {
 
 #Preview {
     ProfileSummary(profile: Profile.default)
+        .environment(ModelData())
 }
